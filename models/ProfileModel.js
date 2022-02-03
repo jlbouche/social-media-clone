35ce35672ef3292c4cfd1ca6f41bd8f0e6fc3f3e
jlbouche <jennalbouche@gmail.com>
2022-02-03 11:14:08 -0800

    create api routes for signup and login

--- models/ProfileModel.js
+++ models/ProfileModel.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const profileSchema = new Schema({
+const ProfileSchema = new Schema({
     user:{type: Schema.Types.ObjectId, ref: "User"},
     bio:{type: String, required:true},
     social: {
-        youtube:{type: String, required:false},
-        twitter:{type: String, required:false},
-        instagram:{type: String, required:false},
-        facebook:{type: String, required:false},
+        youtube:{type: String},
+        twitter:{type: String},
+        instagram:{type: String},
+        facebook:{type: String},
     }
 })
 
