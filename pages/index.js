460e32d83462ee970f787dee25b8b50d3ed6d913
jlbouche <jennalbouche@gmail.com>
2022-02-03 12:41:21 -0800

    setup token authentication with nextjs for signup/login and redirecting in authuser utils

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import baseUrl from '../utils/baseUrl'
 
-export default function Index() {
+export default function Index({user, userFollowStats}) {
   return <div>Home Page</div>;
 }
--- utils/authUser.js
+++ utils/authUser.js
@@ -28,6 +28,15 @@ export const loginUser = async(user, setError, setLoading)=>{
     }
 }
 
+export const redirectUser=(ctx, location)=>{
+    if(ctx.req){
+        ctx.res.writeHead(302, {Location: location})
+        ctx.res.end()
+    } else {
+        Router.push(location);
+    }
+}
+
 const setToken = token => {
     cookie.set('token', token)
 
