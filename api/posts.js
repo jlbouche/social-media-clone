c33ef3817b8704ce67396cb4dc33fc91917db575
jlbouche <jennalbouche@gmail.com>
2022-02-17 10:07:14 -0800

    create api for newlike and removelike notifications

--- api/posts.js
+++ api/posts.js
@@ -5,6 +5,7 @@ const UserModel = require("../models/UserModel");
 const PostModel = require("../models/PostModel");
 const FollowerModel = require("../models/FollowerModel");
 const uuid = require("uuid").v4;
+const {newLikeNotification, removeLikeNotification} = require('../utilsServer/notificationActions')
 
 // CREATE A POST
 
@@ -171,6 +172,10 @@ router.post("/like/:postId", authMiddleware, async (req, res) => {
         await post.likes.unshift({ user: userId });
         await post.save();
 
+        if(post.user.toString()!==userId){
+            await newLikeNotification(userId, postId, post.user.toString())
+        }
+
         return res.status(200).send("Post liked");
     } catch (error) {
         console.error(error);
@@ -202,6 +207,10 @@ router.put("/unlike/:postId", authMiddleware, async (req, res) => {
         await post.likes.splice(index, 1);
 
         await post.save();
+
+        if(post.user.toString()!==userId){
+            await removeLikeNotification(userId, postId, post.user.toString())
+        }
 
         return res.status(200).send("Post Unliked");
     } catch (error) {
