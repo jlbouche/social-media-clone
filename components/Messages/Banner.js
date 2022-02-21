6b4f78964b2399c3a91ba9c68e814a74fdc0a192
jlbouche <jennalbouche@gmail.com>
2022-02-21 12:29:30 -0800

    add utils for userinfo and message sound, create messages components for banner, message, and input field

---
+++ components/Messages/Banner.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Segment, Grid, Image } from "semantic-ui-react";
+
+export default function Banner({ bannerData }) {
+  const { name, profilePicUrl } = bannerData;
+
+  return (
+    <Segment color="teal" attached="top">
+      <Grid>
+        <Grid.Column floated="left" width={14}>
+          <h4>
+            <Image avatar src={profilePicUrl} />
+            {name}
+          </h4>
+        </Grid.Column>
+      </Grid>
+    </Segment>
+  );
+}
