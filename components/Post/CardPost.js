40b2e5a3e64b5bf77c0940bab9888df4b11873d6
jlbouche <jennalbouche@gmail.com>
2022-02-23 11:50:53 -0800

    update post component, index, utilsserver to use sockets to update posts liked/unliked

--- components/Post/CardPost.js
+++ components/Post/CardPost.js
@@ -19,174 +19,186 @@ import LikesList from "./LikesList";
 import ImageModal from "./ImageModal";
 import NoImageModal from "./NoImageModal";
 
-function CardPost({ post, user, setPosts, setShowToastr }) {
-    const [likes, setLikes] = useState(post.likes);
+function CardPost({ post, user, setPosts, setShowToastr, socket }) {
+  const [likes, setLikes] = useState(post.likes);
 
-    const isLiked =
-        likes.length > 0 && likes.filter(like => like.user === user._id).length > 0;
+  const isLiked =
+    likes.length > 0 && likes.filter(like => like.user === user._id).length > 0;
 
-    const [comments, setComments] = useState(post.comments);
+  const [comments, setComments] = useState(post.comments);
 
-    const [error, setError] = useState(null);
+  const [error, setError] = useState(null);
 
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    const addPropsToModal = () => ({
-        post,
-        user,
-        setLikes,
-        likes,
-        isLiked,
-        comments,
-        setComments
-    });
+  const addPropsToModal = () => ({
+    post,
+    user,
+    setLikes,
+    likes,
+    isLiked,
+    comments,
+    setComments
+  });
 
-    return (
-        <>
-        {showModal && (
-            <Modal
-                open={showModal}
-                closeIcon
-                closeOnDimmerClick
-                onClose={() => setShowModal(false)}
-            >
-            <Modal.Content>
-                {post.picUrl ? (
-                <ImageModal {...addPropsToModal()} />
-                ) : (
-                <NoImageModal {...addPropsToModal()} />
-                )}
-            </Modal.Content>
-            </Modal>
-        )}
+  return (
+    <>
+      {showModal && (
+        <Modal
+          open={showModal}
+          closeIcon
+          closeOnDimmerClick
+          onClose={() => setShowModal(false)}
+        >
+          <Modal.Content>
+            {post.picUrl ? (
+              <ImageModal {...addPropsToModal()} />
+            ) : (
+              <NoImageModal {...addPropsToModal()} />
+            )}
+          </Modal.Content>
+        </Modal>
+      )}
 
-        <Segment basic>
-            <Card color="teal" fluid>
-            {post.picUrl && (
-                <Image
-                    src={post.picUrl}
-                    style={{ cursor: "pointer" }}
-                    floated="left"
-                    wrapped
-                    ui={false}
-                    alt="PostImage"
-                    onClick={() => setShowModal(true)}
-                />
+      <Segment basic>
+        <Card color="teal" fluid>
+          {post.picUrl && (
+            <Image
+              src={post.picUrl}
+              style={{ cursor: "pointer" }}
+              floated="left"
+              wrapped
+              ui={false}
+              alt="PostImage"
+              onClick={() => setShowModal(true)}
+            />
+          )}
+
+          <Card.Content>
+            <Image floated="left" src={post.user.profilePicUrl} avatar circular />
+
+            {(user.role === "root" || post.user._id === user._id) && (
+              <>
+                <Popup
+                  on="click"
+                  position="top right"
+                  trigger={
+                    <Image
+                      src="/deleteIcon.svg"
+                      style={{ cursor: "pointer" }}
+                      size="mini"
+                      floated="right"
+                    />
+                  }
+                >
+                  <Header as="h4" content="Are you sure?" />
+                  <p>This action is irreversible!</p>
+
+                  <Button
+                    color="red"
+                    icon="trash"
+                    content="Delete"
+                    onClick={() => deletePost(post._id, setPosts, setShowToastr)}
+                  />
+                </Popup>
+              </>
             )}
 
-            <Card.Content>
-                <Image floated="left" src={post.user.profilePicUrl} avatar circular />
+            <Card.Header>
+              <Link href={`/${post.user.username}`}>
+                <a>{post.user.name}</a>
+              </Link>
+            </Card.Header>
 
-                {(user.role === "root" || post.user._id === user._id) && (
-                <>
-                    <Popup
-                    on="click"
-                    position="top right"
-                    trigger={
-                        <Image
-                            src="/deleteIcon.svg"
-                            style={{ cursor: "pointer" }}
-                            size="mini"
-                            floated="right"
-                        />
-                    }>
-                    <Header as="h4" content="Are you sure?" />
-                    <p>This action is irreversible!</p>
+            <Card.Meta>{calculateTime(post.createdAt)}</Card.Meta>
 
-                    <Button
-                        color="red"
-                        icon="trash"
-                        content="Delete"
-                        onClick={() => deletePost(post._id, setPosts, setShowToastr)}
-                    />
-                    </Popup>
-                </>
-                )}
+            {post.location && <Card.Meta content={post.location} />}
 
-                <Card.Header>
-                <Link href={`/${post.user.username}`}>
-                    <a>{post.user.name}</a>
-                </Link>
-                </Card.Header>
+            <Card.Description
+              style={{
+                fontSize: "17px",
+                letterSpacing: "0.1px",
+                wordSpacing: "0.35px"
+              }}
+            >
+              {post.text}
+            </Card.Description>
+          </Card.Content>
 
-                <Card.Meta>{calculateTime(post.createdAt)}</Card.Meta>
+          <Card.Content extra>
+            <Icon
+              name={isLiked ? "heart" : "heart outline"}
+              color="red"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                if (socket.current) {
+                  socket.current.emit("likePost", {
+                    postId: post._id,
+                    userId: user._id,
+                    like: isLiked ? false : true
+                  });
 
-                {post.location && <Card.Meta content={post.location} />}
-
-                <Card.Description
-                style={{
-                    fontSize: "17px",
-                    letterSpacing: "0.1px",
-                    wordSpacing: "0.35px"
-                }}>
-                {post.text}
-                </Card.Description>
-            </Card.Content>
-
-            <Card.Content extra>
-                <Icon
-                name={isLiked ? "heart" : "heart outline"}
-                color="red"
-                style={{ cursor: "pointer" }}
-                onClick={() =>
-                    likePost(post._id, user._id, setLikes, isLiked ? false : true)
-                }
-                />
-
-                <LikesList
-                    postId={post._id}
-                    trigger={
-                        likes.length > 0 && (
-                        <span className="spanLikesList">
-                            {`${likes.length} ${likes.length === 1 ? "like" : "likes"}`}
-                        </span>
-                        )
+                  socket.current.on("postLiked", () => {
+                    if (isLiked) {
+                      setLikes(prev => prev.filter(like => like.user !== user._id));
                     }
-                />
+                    //
+                    else {
+                      setLikes(prev => [...prev, { user: user._id }]);
+                    }
+                  });
+                } else {
+                  likePost(post._id, user._id, setLikes, isLiked ? false : true);
+                }
+              }}
+            />
 
-                <Icon
-                name="comment outline"
-                style={{ marginLeft: "7px" }}
-                color="blue"
-                />
+            <LikesList
+              postId={post._id}
+              trigger={
+                likes.length > 0 && (
+                  <span className="spanLikesList">
+                    {`${likes.length} ${likes.length === 1 ? "like" : "likes"}`}
+                  </span>
+                )
+              }
+            />
 
-                {comments.length > 0 &&
-                comments.map(
-                    (comment, i) =>
-                    i < 3 && (
-                        <PostComments
-                        key={comment._id}
-                        comment={comment}
-                        postId={post._id}
-                        user={user}
-                        setComments={setComments}
-                        />
-                    )
-                )}
+            <Icon name="comment outline" style={{ marginLeft: "7px" }} color="blue" />
 
-                {comments.length > 3 && (
-                <Button
-                    content="View More"
-                    color="teal"
-                    basic
-                    circular
-                    onClick={() => setShowModal(true)}
-                />
-                )}
+            {comments.length > 0 &&
+              comments.map(
+                (comment, i) =>
+                  i < 3 && (
+                    <PostComments
+                      key={comment._id}
+                      comment={comment}
+                      postId={post._id}
+                      user={user}
+                      setComments={setComments}
+                    />
+                  )
+              )}
 
-                <Divider hidden />
+            {comments.length > 3 && (
+              <Button
+                content="View More"
+                color="teal"
+                basic
+                circular
+                onClick={() => setShowModal(true)}
+              />
+            )}
 
-                <CommentInputField
-                user={user}
-                postId={post._id}
-                setComments={setComments}
-                />
-            </Card.Content>
-            </Card>
-        </Segment>
-        <Divider hidden />
-        </>
-    );
+            <Divider hidden />
+
+            <CommentInputField user={user} postId={post._id} setComments={setComments} />
+          </Card.Content>
+        </Card>
+      </Segment>
+      <Divider hidden />
+    </>
+  );
 }
 
 export default CardPost;
